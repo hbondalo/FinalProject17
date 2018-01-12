f6bfe6c463eae10b8d78f2bb4cf6323ab4d73820
Hannah <hbondalo@ucvts.org>
2018-01-12 17:44:18 +0000

    Save all changes

---
+++ FinalProject17/FinalProject17.py
@@ -0,0 +1,22 @@
+
+n = input("Welcome to your worst nightmare. It's time to escape Magnet! Are you ready? ")
+
+if n == "yes":
+    print("It doesn't matter. You're going to play. Let's begin the journey.\n")
+else:
+    print("It doesn't matter. You're going to play. Let's begin the journny.\n")
+
+print("""Good morning! I have some good news and some bad news. Congratulations! Today's graduation!
+Bad news is that you're stuck in Magnet, and you need to figure out a way to get to the AIT gym in time for graduation!
+You don't want to be THAT kid.
+
+
+BRINNNGGGGG!! BRINGGGGGGGG!!
+
+That's the bell!""")
+
+print("""1. Go to class.
+2. Run to the bathroom.
+3. Pull the fire alarm.""")
+x = input("Choose which number you want to choose. ")
+
